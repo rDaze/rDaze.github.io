9648f00b6e5492e6eb38bd03e1cea1b4a9f02e71
daze <dazesup@gmail.com>
2021-06-01 22:45:07 +0200

    initial autoplay implementation

---
+++ index.js
@@ -0,0 +1,9 @@
+// JavaScript Document
+
+window.onload = function() {
+	document.getElementById("video_killer").play();
+}
+
+window.onclick = function() {
+	document.getElementById("video").play();
+}
