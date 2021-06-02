7e1eed36b6e07046a0e4117b17c37b8e70ec8bb8
daze <dazesup@gmail.com>
2021-06-02 02:55:38 +0200

    added copy function

--- index.js
+++ index.js
@@ -1,9 +1,23 @@
 // JavaScript Document
 
-window.onload = function() {
-	document.getElementById("video_killer").play();
-}
-
-window.onclick = function() {
+document.onload = function () {
 	document.getElementById("video").play();
 }
+
+window.onclick = function () {
+	document.getElementById("video").play();
+}
+
+function copy(value) {
+  var tempInput = document.createElement("input");
+  tempInput.value = value;
+  document.body.appendChild(tempInput);
+  tempInput.select();
+  document.execCommand("copy");
+  document.body.removeChild(tempInput);
+}
+
+document.addEventListener('click', () => {
+	var video = document.getElementById("video");
+	video.muted = !video.muted;
+});
